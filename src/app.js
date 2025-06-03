c392ca3b77c39d93ccd2c2367ca6514837f9184f
fratza <fratzantigua@gmail.com>
2025-06-03 15:35:40 +0800

    cors option

--- src/app.js
+++ src/app.js
@@ -22,8 +22,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/** Enable CORS with default options */
-app.use(cors());
+/** Enable CORS for all origins */
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Handle OPTIONS preflight requests
+app.options('*', cors());
 
 /** Middleware to log requests */
 app.use('/', (req, res, next) => {
